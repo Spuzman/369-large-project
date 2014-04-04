f0151c15018d95775b073fb24025bf2088a4c155
Alex Milbrandt <amilbrandt@wisc.edu>
2014-04-03 20:44:34 -0500

    Pass both cucumber tests

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -6,16 +6,16 @@ When(/^I follow "(.*?)"$/) do |link_text|
   click_link link_text
 end
 
-Then(/^I am on the new book form$/) do
-  current_path.should == new_book_path
+Then(/^I am on the new movement form$/) do
+  current_path.should == new_movement_path
 end
 
-When(/^I am on the show page for that book$/) do
-  book = Book.last
-  book.should_not be_nil
-  visit book_path(book)
+When(/^I am on the show page for that movement$/) do
+  movement = Movement.last
+  movement.should_not be_nil
+  visit movement_path(movement)
 end
 
-When(/^I am on the books index page$/) do
-  visit books_path
-end
+#When(/^I am on the books index page$/) do
+#  visit books_path
+#end
