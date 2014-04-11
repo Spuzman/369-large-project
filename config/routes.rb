b99efef6ba1d03e168177dbc8c6c42ff2d90966e
Alex Milbrandt <amilbrandt@wisc.edu>
2014-04-10 20:02:51 -0500

    Create and pass tests on a static home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 LargeProject::Application.routes.draw do
+  get "static_pages/home"
   resources :artists
 
   resources :movements
-  root 'movements#index'
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
