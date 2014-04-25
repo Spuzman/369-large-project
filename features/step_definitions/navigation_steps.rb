31f63c6967ad3fd82bc683edbf3be7ee4b6d1d4e
Alex Milbrandt <amilbrandt@wisc.edu>
2014-04-24 19:39:33 -0500

    Create cucumber tests and define them

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -2,6 +2,10 @@ Given(/^I am on the home page$/) do
   visit root_path
 end
 
+Given(/^I am on the work page$/) do
+  visit works_path
+end
+
 Given(/^I am on the artist page$/) do
   visit artists_path
 end
@@ -26,6 +30,10 @@ Then(/^I am on the new artist form$/) do
   current_path.should == new_artist_path
 end
 
+Then(/^I am on the new work form$/) do
+  current_path.should == new_work_path
+end
+
 When(/^I am on the show page for that movement$/) do
   movement = Movement.last
   movement.should_not be_nil
@@ -36,5 +44,11 @@ When(/^I am on the show page for that artist$/) do
   artist = Artist.last
   artist.should_not be_nil
   visit artist_path(artist)
+end
+
+When(/^I am on the show page for that work$/) do
+  work = Work.last
+  work.should_not be_nil
+  visit work_path(work)
 end
 
--- features/step_definitions/page_content_steps.rb
+++ features/step_definitions/page_content_steps.rb
@@ -1,3 +1,7 @@
 Then(/^I should see "(.*?)"$/) do |desired_content|
   page.should have_content(desired_content)
 end
+
+Then(/^I should see image "(.*?)"$/) do |image|
+  page.should have_xpath("//img[@src=\"#{image}\"]")
+end
